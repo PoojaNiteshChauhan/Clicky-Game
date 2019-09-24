97fe257b1dbc01513263105d0cd714b1a6555bd0
Pooja Chauhan <PoojaNiteshChauhan.gmail>
2019-09-23 23:38:41 -0400

    push

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,48 +1,134 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+
 import Wrapper from "./components/Wrapper";
 
 import Navbar from "./components/Navbar";
-import friends from "./friends.json";
 
+
+import MatchCard from "./components/MatchCard";
+
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
+
+var correctGuesses = 0;
+var bestScore = 0;
+var clickMessage = "Click on an image to earn points, but don't click on any of them more than once!";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends
-  };
+    
+    // Setting this.state.matches to the matches json array
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+    setClicked = id => {
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <body>
-      <Navbar/>
-      <h1>Clicky Game!
-Click on an image to earn points, but don't click on any more than once!</h1>
-      <Wrapper>
+        // Make a copy of the state matches array to work with
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        // If the matched image's clicked value is already true, 
+        // do the game over actions
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "Dang! You already clicked on that one! Now you have to start over!"
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        // Otherwise, if clicked = false, and the user hasn't finished
+        } else if (correctGuesses < 11) {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // increment the appropriate counter
+            correctGuesses++;
+            
+            clickMessage = "Great! You haven't click on that one yet! Keep going!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // restart the guess counter
+            correctGuesses = 0;
+
+            // Egg on the user to play again
+            clickMessage = "Good Work";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Navbar/>
+                <Title>Welcome to the click Game!</Title>
         
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-      </body>
-    );
-  }
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
